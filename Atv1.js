fdc7089e4e04c28235bc782475bdee56acd6a273
Brendha <oros7090228@gmail.com>
2025-04-17 16:24:50 -0300

    Atualizacao 17-04-2025

--- Atv1.js
+++ Atv1.js
@@ -1,3 +1,17 @@
+const titulos = document.querySelectorAll("li h4");
+const menu1 = document.querySelector(".menu");
+
 function menu(){
-    
+    menu1.classList.toggle("menuOpen");
 }
+
+titulos.forEach((titulo) => {
+    titulo.addEventListener("mouseover", () => {
+        const img = titulo.parentElement.querySelector(".img-opala-foda");
+        img.classList.add("opala-aberto");
+    });
+    titulo.addEventListener("mouseleave", () => {
+        const img2 = titulo.parentElement.querySelector(".img-opala-foda");
+        img2.classList.remove("opala-aberto");
+    });
+});
